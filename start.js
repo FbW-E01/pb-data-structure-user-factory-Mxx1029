c7621e29db8e7fa736c0d3d81d8220bb2f334449
Mxx1029 <lia.pim@icloud.com>
2021-04-28 15:38:55 +0200

    Initial import

--- start.js
+++ start.js
@@ -15,3 +15,31 @@ const userDataDump = [
   "D,Cheesedude51,1951,Users",
   "E,cron,,System",
 ];
+
+const createUserObjects = (userDataDump) => {
+  const arrayOfUserObjects = [];
+  for (let i = 0; i < userDataDump.length; i++) {
+    const user = {};
+    const singleUser = userDataDump[i].split(",");
+    // console.log(singleUser); // helper log
+    const createInitials = (fullName) => {
+      let initials = fullName[0];
+      for (j = 1; j < fullName.length; j++) {
+        if (fullName[j] === " ") {
+          initials += fullName[j + 1]
+        };
+      }
+      return initials.toUpperCase();
+    };
+    user.id = singleUser[0];
+    user.fullName = singleUser[1];
+    user.initials = createInitials(singleUser[1]);
+    user.birthYear = singleUser[2] ? singleUser[2] : "Unknown";
+    user.departments = singleUser[3].split("|");
+    arrayOfUserObjects.push(user);
+    // console.log(user); // helper log
+  };
+  return arrayOfUserObjects;
+};
+
+console.log(createUserObjects(userDataDump));
